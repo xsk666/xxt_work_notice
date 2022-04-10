9f230ab9321f3e26a24a924ce281e67ce0c1c559
xsk666 <3104182180@qq.com>
2022-04-10 16:57:30 +0800

    更新未提交作业检查逻辑

--- xxt_Homework_Notice.py
+++ xxt_Homework_Notice.py
@@ -1,5 +1,6 @@
 # %%
 import time
+import datetime
 from pyquery import PyQuery as pq
 import requests
 import json
@@ -15,7 +16,12 @@ def get_course():
     res2 = req.get("https://mooc1-2.chaoxing.com/course/phone/courselistdata?courseFolderId=0&isFiled=0&query=")
     doc = pq(res2.text)
     for i in doc('li').items():
-        print("courseid",i.attr('courseid'),"clazzid:",i.attr('clazzid'), "classname:",i.text())
+        tmp={
+            "courseid":i.attr("courseid"),
+            "clazzid":i.attr("clazzid"),
+            "classname":i("dt").text()
+        }
+        print(tmp)
     exit()
 
 # 获取作业详情
@@ -30,10 +36,12 @@ def get_homework(user):
                 f"https://mooc1.chaoxing.com/work/task-list?courseId={classitem['courseid']}&classId={classitem['clazzid']}&vx=1")
             doc2 = pq(res3.text)
             for classinfo in doc2('li').items():
-                status = classinfo("span").text()
-                if status == "未交":
+                status=classinfo("span").items()
+                # 未交且作业未截至的作业有两个span，一个是未交，一个是截止时间
+                # pyquery获取到的是个生成器，所以用了__next__()方法
+                if  len(classinfo("span"))==2 and status.__next__().text() == "未交":
                     num += 1
-                    alltaskinfo += f"{classname} {classinfo('a').text()} {classinfo('p').text()}\n"
+                    alltaskinfo += f"\n{classname} {classinfo('a').text()}:{classinfo('p').text()}\n  - {status.__next__().text()}"
                     # print(classname, classinfo("p").text())
         except:
             alltaskinfo += f"{classname} 查询失败\n"
@@ -47,13 +55,12 @@ def get_homework(user):
         print(alltaskinfo)
         # try:
         #     requests.get(
-        #         "http://127.0.0.1:8082/send_private_msg?user_id="+user["qq"]+"&message="+alltaskinfo)
-        #      print("发送通知成功")
+        #         "http://127.0.0.1:8082/send_private_msg?user_id="+user["qq"]+"&message=学习通作业别忘了哦"+alltaskinfo)
+        #     print("发送通知成功")
         # except:
         #     print("发送通知失败")
         print()
-            
-            
+                    
 if __name__ == "__main__":
     try:
         userdata = json.load(open("./data.json", 'r', encoding='utf8'))
@@ -75,6 +82,7 @@ if __name__ == "__main__":
                 # get_course()
                 # 获取作业信息
                 get_homework(user)
+                
                 
     except Exception as e:
         print("登陆失败", e)
